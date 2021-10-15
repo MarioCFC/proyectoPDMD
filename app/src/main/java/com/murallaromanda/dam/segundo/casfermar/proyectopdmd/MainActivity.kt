7eb3fab6bf4bf9bf0d664517680d5f79be3a3bb9
Mario Cascudo <mariocscdclase@gmail.com>
2021-10-15 13:22:23 +0200

    Diseño del login, falta la programacion de los eventos. Creacion de la pantalla de registro, no tiene diseño, y del evento para cambiar de vista.

--- app/src/main/java/com/murallaromanda/dam/segundo/casfermar/proyectopdmd/MainActivity.kt
+++ app/src/main/java/com/murallaromanda/dam/segundo/casfermar/proyectopdmd/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.murallaromanda.dam.segundo.casfermar.proyectopdmd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
+import android.util.Log
+import android.view.inputmethod.InputBinding
+import com.murallaromanda.dam.segundo.casfermar.proyectopdmd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //Enlazamiento de los elementos de la vista
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvCreala.setOnClickListener(){
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.bLogin.setOnClickListener(){
+            var email = binding.tieEmail.text.toString()
+            var password = binding.tiePassword.text.toString()
+
+            Log.d("MainActivity","\nEmail: " + email + "\nPassword: " + password)
+        }
+
     }
 }
