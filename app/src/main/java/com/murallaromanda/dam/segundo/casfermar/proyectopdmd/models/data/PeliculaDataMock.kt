c2cb96b11c1f283c079aa42d63292d2920121459
Mario Cascudo <mariocscdclase@gmail.com>
2021-11-02 12:05:59 +0100

    Seguimos con la lista, hay algun sitio donde da error la imagen, a priori no es por el link

--- app/src/main/java/com/murallaromanda/dam/segundo/casfermar/proyectopdmd/models/data/PeliculaDataMock.kt
+++ app/src/main/java/com/murallaromanda/dam/segundo/casfermar/proyectopdmd/models/data/PeliculaDataMock.kt
@@ -5,7 +5,7 @@ import com.murallaromanda.dam.segundo.casfermar.proyectopdmd.models.entities.Pel
 class PeliculaDataMock : PeliculaData{
     override fun getLista(): List<Pelicula> {
         return listOf(
-            Pelicula("Avatar","Ciencia ficcion","Jame Cameron","8","https://pics.filmaffinity.com/avatar-208925608-mmed.jpg"),
+            Pelicula("El juego de Ender","Ciencia ficcion","Jame Cameron","8","https://res.cloudinary.com/practicaldev/image/fetch/s--sWV8Y0kc--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/kml9j34p9taplrnqtcez.jpg"),
             Pelicula("El castillo ambulante","Fantasia","Hayao Miyazaki","7","https://pics.filmaffinity.com/hauru_no_ugoku_shiro-376386887-mmed.jpg"),
             Pelicula("Shrek","Comedia","Andrew Adamson","9","https://pics.filmaffinity.com/shrek-903764423-mmed.jpg")
         )
