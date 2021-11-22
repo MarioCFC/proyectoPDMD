f85f801a991a0fa6a4393813653d1d872ec8d8b5
Mario Cascudo <Mariocscdclase@gmail.com>
2021-11-22 13:45:38 +0100

    Cambiamos el scroll por un "Mostrar mas"

--- app/src/main/java/com/murallaromanda/dam/segundo/casfermar/proyectopdmd/activities/FilmDetailActivity.kt
+++ app/src/main/java/com/murallaromanda/dam/segundo/casfermar/proyectopdmd/activities/FilmDetailActivity.kt
@@ -10,6 +10,14 @@ import com.murallaromanda.dam.segundo.casfermar.proyectopdmd.databinding.Collaps
 import com.murallaromanda.dam.segundo.casfermar.proyectopdmd.models.entities.Pelicula
 import com.squareup.picasso.Picasso
 import android.view.View.OnTouchListener
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+import androidx.core.content.ContextCompat
+
+import android.animation.ObjectAnimator
+
 
 
 
@@ -18,8 +26,10 @@ class FilmDetailActivity() : AppCompatActivity() {
     private lateinit var binding: CollapsingToolDetailFilmBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        var interruptorSinopsis:Boolean = false
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_film_detail)
+        setContentView(com.murallaromanda.dam.segundo.casfermar.proyectopdmd.R.layout.activity_film_detail)
 
         binding = CollapsingToolDetailFilmBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -36,12 +46,7 @@ class FilmDetailActivity() : AppCompatActivity() {
         Picasso.get().load(pelicula.urlBanner).into(binding.collapsingToolbarImagenFondo)
 
         //Evento de toque sobre el textView
-        binding.layoutDetallesPeliculaCollapse.FilmDetailTvSinopsis.setOnTouchListener { v, event ->
-            if (event.action == MotionEvent.ACTION_UP || event.action == MotionEvent.ACTION_DOWN) {
 
-                true
-            } else false
-        }
 
     }
 }
