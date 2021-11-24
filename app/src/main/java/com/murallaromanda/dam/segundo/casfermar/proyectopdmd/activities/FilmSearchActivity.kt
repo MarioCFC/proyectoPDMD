5fb7182855ea0700e9bb8fe191554bf85421fdcf
Mario Cascudo <mariocscdclase@gmail.com>
2021-11-24 13:10:07 +0100

    Intentando borrar el boton flotante del buscador

--- app/src/main/java/com/murallaromanda/dam/segundo/casfermar/proyectopdmd/activities/FilmSearchActivity.kt
+++ app/src/main/java/com/murallaromanda/dam/segundo/casfermar/proyectopdmd/activities/FilmSearchActivity.kt
@@ -3,10 +3,14 @@ package com.murallaromanda.dam.segundo.casfermar.proyectopdmd.activities
 import android.app.SearchManager
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.view.animation.OvershootInterpolator
 import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.GridLayoutManager
+import com.github.clans.fab.FloatingActionButton
 import com.murallaromanda.dam.segundo.casfermar.proyectopdmd.R
 import com.murallaromanda.dam.segundo.casfermar.proyectopdmd.adapters.ListaPeliculasAdapter
 import com.murallaromanda.dam.segundo.casfermar.proyectopdmd.databinding.ActivityDatabaseFilmSearcBinding
@@ -34,14 +38,9 @@ class FilmSearchActivity: AppCompatActivity() {
         binding.layoutDetallesPeliculaCollapse.rvListaPeliculas.layoutManager = layoutManager
         binding.layoutDetallesPeliculaCollapse.rvListaPeliculas.adapter = adapter
 
-
-
         var searchView = binding.searchView as androidx.appcompat.widget.SearchView
         searchView.setQueryHint("Search View Hint")
         searchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
-
-
-
 
             override fun onQueryTextChange(newText: String): Boolean {
                 return false
